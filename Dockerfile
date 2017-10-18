1a7c67b4c275e3e4cd8cfc55bfba9775f3a3cbd2
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2017-10-19 00:28:39 +0200

    Update script handling

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM node:alpine
 ENV SCRIPT "console.log('env SCRIPT not defined')"
 
 COPY run.js run.js
-COPY loader.js loader.js
 COPY package.json package.json
 
 RUN npm install
--- run.js
+++ run.js
@@ -20,21 +20,19 @@ RegExp.prototype.execAll = function(string) {
 
 let script = process.env.SCRIPT;
 
-script = script;
-
 fs.writeFileSync("./script.js", script);
 
 const ignoreInstallationForModules = builtinModules;
 const matches = requireRegexp.execAll(script);
 matches.forEach(match => {
-  console.log("match", match[1].replace(/'/g, '"'));
+  // console.log("match", match[1].replace(/'/g, '"'));
   const moduleName = JSON.parse(match[1].replace(/'/g, '"'));
 
   if (ignoreInstallationForModules.indexOf(moduleName) != -1) return;
 
   console.log(`installing dependency ${moduleName}`);
-  const result = execSync(`npm install ${moduleName}`);
-  console.log(`installed ${moduleName}\nlogs: ${result}`);
+  const result = execSync(`npm install ${moduleName} > /dev/null`);
+  // console.log(`installed ${moduleName}\nlogs: ${result}`);
 });
 
 require("./script.js");
